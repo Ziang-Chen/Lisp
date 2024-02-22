4b5dc186c1ad928f6089c9671b67cb127c60ed25
Ziang-Chen <78735455+Ziang-Chen@users.noreply.github.com>
2024-02-22 10:25:25 +0100

    add cppExportor

---
+++ Tools/cppExportor.py
@@ -0,0 +1,11 @@
+from Compiler.Context import Context
+import Compiler.Symbol as Symbol
+import typing
+from Compiler.ContextManager import ContextManager
+import Grammer.Tokens as Tokens
+import operator as op
+import Grammer.BNF as bnf
+import Grammer.Sematics as sem
+from Parser.lispStyle import parser
+
+
